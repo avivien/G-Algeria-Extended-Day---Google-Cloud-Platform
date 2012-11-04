286943e9480e9bf020193369b995e9b955a4db68
Alexandre Vivien <alx.vivien@gmail.com>
2012-11-04 23:24:22 +0100

    First online version

---
+++ slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'G|Algeria Extended day 2012',
+    subtitle: 'Google Cloud Platform',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_algeria_extended_day.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Alexandre Vivien',
+    company: 'Cloud Computing Architect, Capgemini',
+    twitter: '@alexandrevivien',
+    gplus: 'https://plus.google.com/101227802782017377502'
+  }, {
+    name: 'Abdellah Boussaidi',
+    company: 'Cloud Computing Technical Consultant, Capgemini',
+  }, {
+    name: 'Nesrine Louni',
+    company: 'Cloud Computing Technical Consultant, Capgemini',
+  }]
+};
+
